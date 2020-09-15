26a7035620b64e5b6256a8fe6c3021e6ce56c287
Joon-Erik Sæther <jooneris@stud.ntnu.no>
2020-09-15 12:50:39 +0000

    add controller

---
+++ gr2038/src/main/java/dinnerium/ui/AppController.java
@@ -0,0 +1,15 @@
+package dinnerium.ui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AppController {
+    
+    @FXML
+    Button clickMeButton;
+
+    @FXML
+    void handleClickMeButtonAction() {
+        clickMeButton.setText("Thanks!");
+    }
+}
