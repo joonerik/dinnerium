35cefc8a72730ff1798d81409cdda890b536c326
AndersRobstad <54552345+AndersRobstad@users.noreply.github.com>
2020-09-16 13:56:38 +0200

    add fxid and fields in controller

--- gr2038/src/main/java/dinnerium/ui/AppController.java
+++ gr2038/src/main/java/dinnerium/ui/AppController.java
@@ -1,9 +1,35 @@
 package dinnerium.ui;
 
-//import javafx.fxml.FXML;
-//import javafx.scene.control.Button;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class AppController {
-    
-  
+
+    @FXML
+    TextField nameInput;
+    @FXML
+    TextField quantityInput;
+    @FXML
+    ComboBox unitComboBox;
+    @FXML
+    Button addButton;
+    @FXML
+    TableView<String> ingredientTableView;
+    @FXML
+    TableColumn quantityColumn;
+    @FXML
+    TableColumn itemColumn;
+
+
+
+
+    @FXML
+    private void initialize() {
+
+    }
+
+    @FXML
+    private void addIngredient() {
+
+    }
 }
