cbfd57fe3ff774cebbbea3d75b80cd473ff7d4a9
Anders Robstad <anderobs@stud.ntnu.no>
2020-09-25 07:33:21 +0000

    add getters and setters

--- gr2038/core/src/main/java/dinnerium/core/Metadata.java
+++ gr2038/core/src/main/java/dinnerium/core/Metadata.java
@@ -5,4 +5,16 @@ public class Metadata {
     private String author;
     private double portion;
 
+    public Metadata(String author, double portion) {
+        this.author = author;
+        this.portion = portion;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+    public Double getPortion() {
+        return portion;
+    }
+
 }
