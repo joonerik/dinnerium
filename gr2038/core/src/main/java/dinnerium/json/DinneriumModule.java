bb363e6daf61ad4650248d9db5105be4c59627c4
AndersRobstad <54552345+AndersRobstad@users.noreply.github.com>
2020-09-30 12:21:54 +0200

    add RecipeDeserializer class

--- gr2038/core/src/main/java/dinnerium/json/DinneriumModule.java
+++ gr2038/core/src/main/java/dinnerium/json/DinneriumModule.java
@@ -2,10 +2,7 @@ package dinnerium.json;
 
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.module.SimpleModule;
-import dinnerium.core.Ingredient;
-import dinnerium.core.IngredientContainer;
-import dinnerium.core.Metadata;
-import dinnerium.core.Quantity;
+import dinnerium.core.*;
 
 @SuppressWarnings("serial")
 public class DinneriumModule extends SimpleModule {
@@ -17,12 +14,11 @@ public class DinneriumModule extends SimpleModule {
         super(NAME, Version.unknownVersion());
         addSerializer(Ingredient.class, new IngredientSerializer());
         addSerializer(Quantity.class, new QuantitySerializer());
-        addSerializer(IngredientContainer.class, new IngredientContainerSerializer());
+        addSerializer(Container.class, new ContainerSerializer());
         addSerializer(Metadata.class, new MetadataSerializer());
         addDeserializer(Ingredient.class, new IngredientDeserializer());
         addDeserializer(Quantity.class, new QuantityDeserializer());
-        addDeserializer(IngredientContainer.class, new IngredientContainerDeserializer());
+        addDeserializer(Container.class, new ContainerDeserializer());
         addDeserializer(Metadata.class, new MetadataDeserializer());
-
     }
 }
--- gr2038/core/src/main/java/dinnerium/json/RecipeSerializer.java
+++ gr2038/core/src/main/java/dinnerium/json/RecipeSerializer.java
@@ -1,4 +1,22 @@
 package dinnerium.json;
 
-public class RecipeSerializer {
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import dinnerium.core.Recipe;
+
+import java.io.IOException;
+
+public class RecipeSerializer extends JsonSerializer<Recipe> {
+
+    @Override
+    public void serialize(Recipe recipe,
+                          JsonGenerator jsonGen,
+                          SerializerProvider serializerProvider) throws IOException {
+        jsonGen.writeStartObject();
+        jsonGen.writeObjectField("ingredientContainer", recipe.getIngredientContainer());
+        jsonGen.writeObjectField("recipeInstructions", recipe.getRecipeInstructions());
+        jsonGen.writeObjectField("metadata", recipe.getMetadata());
+        jsonGen.writeEndObject();
+    }
 }
