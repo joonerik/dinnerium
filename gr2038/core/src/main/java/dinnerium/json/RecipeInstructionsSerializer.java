4b90566b13eb4e9a1457e3c78c36a116ccc13f2f
Joon-Erik Sæther <joon-erik@hotmail.com>
2020-09-30 14:00:40 +0200

    add recipeInstructionsSerializer

---
+++ gr2038/core/src/main/java/dinnerium/json/RecipeInstructionsSerializer.java
@@ -0,0 +1,32 @@
+package dinnerium.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import dinnerium.core.RecipeInstructions;
+import java.io.IOException;
+
+public class RecipeInstructionsSerializer extends JsonSerializer<RecipeInstructions> {
+
+
+    // converts the Quantity object to a string in json format
+    @Override
+    public void serialize(
+            RecipeInstructions recipeInstructions,
+            JsonGenerator jsonGen,
+            SerializerProvider serializerProvider) throws IOException {
+
+        jsonGen.writeStartObject();
+        for (String instruction : recipeInstructions) {
+            jsonGen.writeStringField("recipeInstructions", instruction);
+        }
+
+        /*jsonGen.writeArrayFieldStart("recipeInstructions");
+        for (String instruction : recipeInstructions) {
+            jsonGen.writeString(instruction);
+        }
+        jsonGen.writeEndArray();*/
+
+        jsonGen.writeEndObject();
+    }
+}
