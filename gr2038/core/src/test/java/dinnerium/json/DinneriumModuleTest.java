a70d1f1f92c9e8d9f8484fc3be103e3ff57d856e
AndersRobstad <54552345+AndersRobstad@users.noreply.github.com>
2020-10-05 19:50:49 +0200

    update serializer/deserializer tests

--- gr2038/core/src/test/java/dinnerium/json/DinneriumModuleTest.java
+++ gr2038/core/src/test/java/dinnerium/json/DinneriumModuleTest.java
@@ -1,39 +1,253 @@
 package dinnerium.json;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dinnerium.core.Ingredient;
 import dinnerium.core.IngredientContainer;
+import dinnerium.core.Metadata;
 import dinnerium.core.Quantity;
+import dinnerium.core.Recipe;
+import dinnerium.core.RecipeContainer;
+import dinnerium.core.RecipeInstructions;
+import dinnerium.core.User;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Iterator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class DinneriumModuleTest {
 
     private ObjectMapper mapper;
-    private String exampleIngredientContainer = "" +
-            "{\"ingredients\":[" +
-            "{\"quantity\":{\"unit\":\"stk\",\"amount\":10.0},\"name\":\"egg\"}," +
-            "{\"quantity\":{\"unit\":\"gram\",\"amount\":500.0},\"name\":\"mel\"}," +
-            "{\"quantity\":{\"unit\":\"dl\",\"amount\":5.0},\"name\":\"melk\"}" +
-            "]}";
+    //Fiks formateringen på recipeInstructions!!!
+    private String expectedUserString = "" +
+        "{\n" +
+        "  \"ingredientContainer\" : {\n" +
+        "    \"container\" : [ {\n" +
+        "      \"quantity\" : {\n" +
+        "        \"amount\" : 1.0,\n" +
+        "        \"unit\" : \"stk\"\n" +
+        "      },\n" +
+        "      \"name\" : \"eggs\"\n" +
+        "    }, {\n" +
+        "      \"quantity\" : {\n" +
+        "        \"amount\" : 2.0,\n" +
+        "        \"unit\" : \"dl\"\n" +
+        "      },\n" +
+        "      \"name\" : \"milk\"\n" +
+        "    }, {\n" +
+        "      \"quantity\" : {\n" +
+        "        \"amount\" : 3.0,\n" +
+        "        \"unit\" : \"gram\"\n" +
+        "      },\n" +
+        "      \"name\" : \"sugar\"\n" +
+        "    } ],\n" +
+        "    \"containerSize\" : 3\n" +
+        "  },\n" +
+        "  \"recipeContainer\" : {\n" +
+        "    \"container\" : [ {\n" +
+        "      \"ingredientContainer\" : {\n" +
+        "        \"container\" : [ {\n" +
+        "          \"quantity\" : {\n" +
+        "            \"amount\" : 400.0,\n" +
+        "            \"unit\" : \"gram\"\n" +
+        "          },\n" +
+        "          \"name\" : \"minced meat\"\n" +
+        "        }, {\n" +
+        "          \"quantity\" : {\n" +
+        "            \"amount\" : 200.0,\n" +
+        "            \"unit\" : \"gram\"\n" +
+        "          },\n" +
+        "          \"name\" : \"cheese\"\n" +
+        "        }, {\n" +
+        "          \"quantity\" : {\n" +
+        "            \"amount\" : 9.0,\n" +
+        "            \"unit\" : \"stk\"\n" +
+        "          },\n" +
+        "          \"name\" : \"lasagne plates\"\n" +
+        "        } ],\n" +
+        "        \"containerSize\" : 3\n" +
+        "      },\n" +
+        "      \"recipeInstructions\" : {\n" +
+        "        \"instructions\" : [ \"cook\", \"bake\", \"eat\" ]\n" +
+        "      },\n" +
+        "      \"metadata\" : {\n" +
+        "        \"author\" : \"bestUsername\",\n" +
+        "        \"portion\" : 4.0,\n" +
+        "        \"image\" : \"http://folk.ntnu.no/anderobs/images/tikkaMasala.png\",\n" +
+        "        \"recipeName\" : \"Lasagne\",\n" +
+        "        \"recipeDescription\" : \"God og smakfull lasagne\",\n" +
+        "        \"minutes\" : 90\n" +
+        "      }\n" +
+        "    }, {\n" +
+        "      \"ingredientContainer\" : {\n" +
+        "        \"container\" : [ {\n" +
+        "          \"quantity\" : {\n" +
+        "            \"amount\" : 2.0,\n" +
+        "            \"unit\" : \"stk\"\n" +
+        "          },\n" +
+        "          \"name\" : \"eggs\"\n" +
+        "        }, {\n" +
+        "          \"quantity\" : {\n" +
+        "            \"amount\" : 3.0,\n" +
+        "            \"unit\" : \"dl\"\n" +
+        "          },\n" +
+        "          \"name\" : \"tikka\"\n" +
+        "        } ],\n" +
+        "        \"containerSize\" : 2\n" +
+        "      },\n" +
+        "      \"recipeInstructions\" : {\n" +
+        "        \"instructions\" : [ \"mix\", \"doStuff\", \"serve\" ]\n" +
+        "      },\n" +
+        "      \"metadata\" : {\n" +
+        "        \"author\" : \"bestUsername\",\n" +
+        "        \"portion\" : 2.0,\n" +
+        "        \"image\" : \"http://folk.ntnu.no/anderobs/images/tikkaMasala.png\",\n" +
+        "        \"recipeName\" : \"Tikka masala\",\n" +
+        "        \"recipeDescription\" : \"Describing description of tikka masala\",\n" +
+        "        \"minutes\" : 60\n" +
+        "      }\n" +
+        "    } ],\n" +
+        "    \"containerSize\" : 2\n" +
+        "  },\n" +
+        "  \"username\" : \"bestUsername\"\n" +
+        "}";
 
     @BeforeEach
     void setUp() {
         mapper = new ObjectMapper();
         mapper.registerModule(new DinneriumModule());
     }
+
     @Test
     public void testSerializers() {
+        User expectedUser = createExceptedUser();
+
         assertTrue(true);
+
+        try {
+            String serializedObject =
+                mapper.writerWithDefaultPrettyPrinter().writeValueAsString(expectedUser);
+            assertEquals(expectedUserString.replaceAll("\\s+", ""), serializedObject);
+        } catch (IOException e) {
+            fail("User is not written to file as it is supposed to");
+        }
+    }
+
+    @Test
+    public void testDeserializers() {
+        try {
+            User jsonUser = mapper.readValue(expectedUserString, User.class);
+            User expectedUser = createExceptedUser();
+            compareUsers(expectedUser, jsonUser);
+
+        } catch (IOException e) {
+            fail("Could not deserialize object from file correctly");
+        }
+    }
+
+    private void compareUsers(User expectedUser, User u2) {
+        assertEquals(expectedUser.getUsername(), u2.getUsername());
+        compareIngredientContainers(expectedUser.getIngredientContainer(),
+            u2.getIngredientContainer());
+        compareRecipeContainers(expectedUser.getRecipeContainer(), u2.getRecipeContainer());
+    }
+
+    private void compareIngredientContainers(IngredientContainer expectedIngredientContainer,
+                                             IngredientContainer ic2) {
+        Iterator<Ingredient> expectedIterator =
+            expectedIngredientContainer.getContainer().iterator();
+        Iterator<Ingredient> it2 = ic2.getContainer().iterator();
+
+        assertTrue(it2.hasNext());
+        compareIngredient(expectedIterator.next(), it2.next());
+        assertTrue(it2.hasNext());
+        compareIngredient(expectedIterator.next(), it2.next());
+        assertTrue(it2.hasNext());
+        compareIngredient(expectedIterator.next(), it2.next());
+        assertFalse(it2.hasNext());
+    }
+
+    private void compareRecipeContainers(RecipeContainer expectedRecipeContainer,
+                                         RecipeContainer rc2) {
+        Iterator<Recipe> expectedIterator = expectedRecipeContainer.getContainer().iterator();
+        Iterator<Recipe> it2 = rc2.getContainer().iterator();
+
+        assertTrue(it2.hasNext());
+        compareRecipe(expectedIterator.next(), it2.next());
+        assertTrue(it2.hasNext());
+        compareRecipe(expectedIterator.next(), it2.next());
+        assertFalse(it2.hasNext());
+    }
+    private void compareRecipe(Recipe expectedRecipe, Recipe r2) {
+        compareIngredientContainers(expectedRecipe.getIngredientContainer(), r2.getIngredientContainer());
+        compareRecipeInstructions(expectedRecipe.getRecipeInstructions(), r2.getRecipeInstructions());
+        compareMetadata(expectedRecipe.getMetadata(), r2.getMetadata());
+    }
+    private void compareRecipeInstructions(RecipeInstructions expectedInstructions, RecipeInstructions ri2) {
+        Iterator<String> expectedIterator = expectedInstructions.iterator();
+        Iterator<String> it2 = ri2.iterator();
+
+        assertTrue(it2.hasNext());
+        assertEquals(expectedIterator.next(), it2.next());
+        assertTrue(it2.hasNext());
+        assertEquals(expectedIterator.next(), it2.next());
+        assertTrue(it2.hasNext());
+        assertEquals(expectedIterator.next(), it2.next());
+        assertFalse(it2.hasNext());
+    }
+    private void compareMetadata(Metadata expectedMetadata, Metadata m2) {
+        assertEquals(expectedMetadata.getAuthor(), m2.getAuthor());
+        assertEquals(expectedMetadata.getImage(), m2.getImage());
+        assertEquals(expectedMetadata.getMinutes(), m2.getMinutes());
+        assertEquals(expectedMetadata.getRecipeDescription(), m2.getRecipeDescription());
+        assertEquals(expectedMetadata.getRecipeName(), m2.getRecipeName());
+    }
+
+    private void compareIngredient(Ingredient expectedIngredient, Ingredient i2) {
+        assertEquals(expectedIngredient.getName(), i2.getName());
+        assertEquals(expectedIngredient.getQuantity().getAmount(), i2.getQuantity().getAmount());
+        assertEquals(expectedIngredient.getQuantity().getUnit(), i2.getQuantity().getUnit());
+    }
+
+    private User createExceptedUser() {
+        IngredientContainer ic = new IngredientContainer();
+        ic.addItem(new Ingredient(new Quantity(1, "stk"), "eggs"));
+        ic.addItem(new Ingredient(new Quantity(2, "dl"), "milk"));
+        ic.addItem(new Ingredient(new Quantity(3, "gram"), "sugar"));
+        RecipeContainer rc = createExpectedRecipeContainer();
+
+        return new User(ic, rc, "bestUsername");
+    }
+
+    private RecipeContainer createExpectedRecipeContainer() {
+        RecipeContainer rc = new RecipeContainer();
+        double[][] amounts = {{400, 200, 9}, {2, 3}};
+        String[][] units = {{"gram", "gram", "stk"}, {"stk", "dl"}};
+        String[][] names = {{"minced meat", "cheese", "lasagne plates"}, {"eggs", "tikka"}};
+        String[][] instructions = {{"cook", "bake", "eat"}, {"mix", "doStuff", "serve"}};
+        double[] portions = {4, 2};
+        String[] recipeName = {"Lasagne", "Tikka masala"};
+        String[] descriptions =
+            {"God og smakfull lasagne", "Describing description of tikka masala"};
+        int[] minutes = {90, 60};
+
+        for (int i = 0; i < recipeName.length; i++) {
+            IngredientContainer ic = new IngredientContainer();
+            for (int j = 0; j < amounts[i].length; j++) {
+                ic.addItem(new Ingredient(new Quantity(amounts[i][j], units[i][j]), names[i][j]));
+            }
+            Metadata md = new Metadata("bestUsername", portions[i],
+                "http://folk.ntnu.no/anderobs/images/tikkaMasala.png", recipeName[i],
+                descriptions[i], minutes[i]);
+            rc.addItem(new Recipe(ic, new RecipeInstructions(Arrays.asList(instructions[i])), md));
+        }
+        return rc;
     }
 /*
     @Test
@@ -66,14 +280,6 @@ class DinneriumModuleTest {
         } catch (JsonProcessingException e) {
             fail();
         }
-    }
-
-    private Collection<Ingredient> createSampleIngredient() {
-        List<Ingredient> sampleIngredients = new ArrayList<>();
-        sampleIngredients.add(new Ingredient(new Quantity(10, "stk"), "egg"));
-        sampleIngredients.add(new Ingredient(new Quantity(500, "gram"), "mel"));
-        sampleIngredients.add(new Ingredient(new Quantity(5, "dl"), "melk"));
-
-        return sampleIngredients;
     }*/
+
 }
