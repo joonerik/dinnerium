7a6810be7500b86e85121b74f7e55db45e514bbe
Joon-Erik Sæther <joon-erik@hotmail.com>
2020-10-06 16:48:29 +0200

    finalize IngredientTest

--- gr2038/core/src/test/java/dinnerium/core/IngredientTest.java
+++ gr2038/core/src/test/java/dinnerium/core/IngredientTest.java
@@ -1,45 +1,63 @@
 package dinnerium.core;
 
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class IngredientTest {
 
+    Quantity q = new Quantity(1.0, "dl");
+    Ingredient i = new Ingredient(q, "milk");
+
     @Test
     void getQuantityTest() {
-        Quantity q = new Quantity(1.0, "dl");
-        Ingredient i = new Ingredient(q, "milk");
-
         assertEquals(q, i.getQuantity());
     }
 
     @Test
     void getNameTest() {
-        Quantity q = new Quantity(1.0, "dl");
-        Ingredient i = new Ingredient(q, "milk");
-
         assertEquals("milk", i.getName());
     }
 
     @Test
-    void setNameTest() {
-        Quantity q = new Quantity(1.0, "dl");
-        Ingredient i = new Ingredient(q, "milk");
-
+    void setQuantityTest() {
         Quantity q1 = new Quantity(2.0, "stk");
         i.setQuantity(q1);
-
         assertEquals(q1, i.getQuantity());
+
     }
 
     @Test
-    void setQuantityTest() {
-        Quantity q = new Quantity(1.0, "dl");
-        Ingredient i = new Ingredient(q, "milk");
-
+    void setNameTest() {
         i.setName("flour");
-
         assertEquals("flour", i.getName());
     }
+
+    @Test
+    void testLowerCaseName() {
+        i.setName("Flour");
+        assertEquals("flour", i.getName());
+    }
+
+    @Test
+    void testSetName2() {
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                i.setName("");
+            }
+        });
+
+        assertThrows(IllegalArgumentException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                i.setName("32456");
+            }
+        });
+    }
+
 }
