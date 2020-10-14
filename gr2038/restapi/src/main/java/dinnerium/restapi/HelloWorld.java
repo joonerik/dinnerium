409fd60a290269ff7f7621dc91ded87c13793b68
AndersRobstad <54552345+AndersRobstad@users.noreply.github.com>
2020-10-14 15:30:27 +0200

    add get method for listing all usernames

--- gr2038/restapi/src/main/java/dinnerium/restapi/HelloWorld.java
+++ gr2038/restapi/src/main/java/dinnerium/restapi/HelloWorld.java
@@ -1,21 +1,41 @@
 package dinnerium.restapi;
 
 import static spark.Spark.*;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class HelloWorld {
+
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello, world");
 
         get("/hello/:name", (req, res) -> "Hello, " + req.params(":name"));
 
         get("/users/:name", (req, res) -> getUser(req.params(":name")));
+
+        get("/users", (req, res) -> getUsernameList());
     }
 
     private static String getUser(String param) throws IOException {
         String file = "core/src/main/resources/dinnerium/storage/" + param + ".json";
         return new String(Files.readAllBytes(Paths.get(file)));
+    }
+
+    private static String getUsernameList() {
+        File[] files = new File("core/src/main/resources/dinnerium/storage/").listFiles();
+        List<String> filenames = new ArrayList<>();
+        if (files != null) {
+            filenames = Arrays.stream(files)
+                .map(file -> "\"" + file.getName().replace(".json", "") + "\"")
+                .collect(Collectors.toList());
+        }
+        return "{ \"usernames\" : " + filenames.toString() + "}";
     }
 }
