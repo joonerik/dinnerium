a0ea8292b70ec0ebb1e17e8634335748652e8993
Børge Dreyer <borge.dreyer@juniorconsulting.no>
2020-10-14 15:32:37 +0200

    add scss and restructure

--- gr2038/ui/src/pages/LoginPage.tsx
+++ gr2038/ui/src/pages/LoginPage.tsx
@@ -1,31 +1,11 @@
-import React, { useState } from "react"
+import React from "react"
+import "../assets/styles/defaults.scss"
+import LoginModal from "../components/LoginModal/LoginModal"
 
 const LoginPage = () => {
-  const [isUser, setUser] = useState<string>("")
-
-  const getUser = async (user: string) => {
-    console.log(user)
-    const response = await fetch(`/users/${user}`, {
-      mode: "no-cors",
-    })
-    const data = await response.json()
-    console.log(data)
-  }
   return (
-    <div className="login__modal">
-      <h2> Your Username</h2>
-      <input
-        type="text"
-        name="username"
-        id="usernameInput"
-        required
-        placeholder="Your username"
-        onChange={(event) => setUser(event.target.value)}
-      />
-
-      <button type="submit" onClick={() => getUser(isUser)}>
-        Login
-      </button>
+    <div className="default__page">
+      <LoginModal />
     </div>
   )
 }
