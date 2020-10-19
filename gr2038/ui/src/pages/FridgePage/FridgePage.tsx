cc1f8db1f48d87776c1a69962b36bd1253046b9c
Ruben Kobbeltvedt <rubenkob@stud.ntnu.no>
2020-10-19 11:15:27 +0200

    add an example object to page

--- gr2038/ui/src/pages/FridgePage/FridgePage.tsx
+++ gr2038/ui/src/pages/FridgePage/FridgePage.tsx
@@ -1,10 +1,56 @@
-import React from 'react';
+import React, { FC } from 'react';
 
-const FridgePage = () => {
+import './FridgePage.scss';
+
+interface IItem {
+  item: { name: string };
+}
+
+const Item: FC<IItem> = ({ item }) => {
+  return <div className="item">{item.name}</div>;
+};
+
+function FridgePage() {
+  const items = [
+    {
+      quantity: {
+        unit: 'stk',
+        amount: 2.0,
+      },
+      name: 'bolle',
+    },
+    {
+      quantity: {
+        unit: 'stk',
+        amount: 20.0,
+      },
+      name: 'kanelboller',
+    },
+    {
+      quantity: {
+        unit: 'gram',
+        amount: 200.0,
+      },
+      name: 'kaffe',
+    },
+    {
+      quantity: {
+        unit: 'dl',
+        amount: 40.0,
+      },
+      name: 'melk',
+    },
+  ];
+
   return (
-    <div>
-      <h1>Fridge Page</h1>
+    <div className="fridgePage">
+      <div className="itemList">
+        {items.map((item) => (
+          <Item item={item} />
+        ))}
+      </div>
     </div>
   );
-};
+}
+
 export default FridgePage;
