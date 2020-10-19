202943e195d49f32c65d5038cb914568919aa573
Børge Dreyer <borge.dreyer@gmail.com>
2020-10-19 13:11:23 +0200

    add form

--- gr2038/ui/src/pages/LoginPage/LoginPage.tsx
+++ gr2038/ui/src/pages/LoginPage/LoginPage.tsx
@@ -1,33 +1,53 @@
 import React, { useState } from 'react';
 import '../../assets/styles/defaults.scss';
-
+import axios from 'axios';
+interface IItem {
+  item: { item: { text: string } };
+}
 const LoginPage = () => {
   //const [isLoginStatus, setLoginStatus] = useState(false);
   const [isUser, setUser] = useState<string>('');
+  const [name, setName] = useState<string>('');
+  const [isAction, setAction] = useState<string>('');
 
+  const submitForm = (
+    event: React.FormEvent<HTMLFormElement>,
+    action: String
+  ) => {
+    event.preventDefault();
+    axios.post('/users/' + action, { username: name }).then(function (res) {
+      console.log(res);
+    });
+  };
   const getUser = async (user: string) => {
     console.log(user);
+
     const response = await fetch(`/users/${user}`, {
       mode: 'no-cors',
     });
+
     const data = await response.json();
     console.log(data);
   };
+
   return (
     <div className="login__modal">
-      <h2> Your Username</h2>
-      <input
-        type="text"
-        name="username"
-        id="usernameInput"
-        required
-        placeholder="Your username"
-        onChange={(event) => setUser(event.target.value)}
-      />
-
-      <button type="submit" onClick={() => getUser(isUser)}>
-        Login
-      </button>
+      <form method="post" onSubmit={(e) => submitForm(e, isAction)}>
+        <input
+          onChange={(event) => setName(event.target.value)}
+          placeholder="username"
+          type="text"
+          name="username"
+          required
+        />
+        <br />
+        <input type="submit" value="Login" onClick={() => setAction('login')} />
+        <input
+          type="submit"
+          value="Register"
+          onClick={() => setAction('register')}
+        />
+      </form>
     </div>
   );
 };
