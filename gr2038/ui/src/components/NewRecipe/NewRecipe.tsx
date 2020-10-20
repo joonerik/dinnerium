068e0d7e93f318abfb49329ec916efd9e87dbe4e
AndersRobstad <54552345+AndersRobstad@users.noreply.github.com>
2020-10-20 13:31:55 +0200

    add functions for adding instructions/ingredients to the form data

--- gr2038/ui/src/components/NewRecipe/NewRecipe.tsx
+++ gr2038/ui/src/components/NewRecipe/NewRecipe.tsx
@@ -1,7 +1,26 @@
-import React, { useState } from 'react';
+import React, { createRef } from 'react';
 import './newRecipe.scss';
 
+const instructionTextArea = createRef<HTMLTextAreaElement>();
+const instructionsList = createRef<HTMLOListElement>();
+const ingredientsList = createRef<HTMLOListElement>();
+
 const NewRecipe = () => {
+  const addInstruction = () => {
+    if (instructionTextArea.current?.value) {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = instructionTextArea.current.value;
+      instructionsList.current?.appendChild(listItem);
+    }
+  };
+  const addIngredient = () => {
+    //Check if input fields are all filled out and create element and append text if so.
+    const listItem = document.createElement('li');
+    // Retrive correct data from the fields and add it to the listitem.
+    listItem.innerHTML = 'test :)';
+    ingredientsList.current?.appendChild(listItem);
+  };
+
   return (
     <div className="new-recipe-container">
       <form>
@@ -43,28 +62,33 @@ const NewRecipe = () => {
           <option value="dl">dl</option>
           <option value="stk">stk</option>
         </select>
-        <button type="button" id="addIngredientButton">
+        <button type="button" onClick={addIngredient}>
           Add
         </button>
         <br />
         <textarea
           name="instruction"
           placeholder="Instruction"
+          ref={instructionTextArea}
           required
         ></textarea>
         <br />
-        <button type="button" id="addInstructionButton">
+        <button
+          type="button"
+          id="addInstructionButton"
+          onClick={addInstruction}
+        >
           Add instruction
         </button>
         <br />
         <div className="instructions">
           <p>Instructions</p>
-          <ol id="instructionList"></ol>
+          <ol ref={instructionsList}></ol>
         </div>
         <br />
         <div className="ingredients">
           <p>Ingredients</p>
-          <ol id="ingredientList"></ol>
+          <ol ref={ingredientsList}></ol>
         </div>
         <br />
         <input type="submit" value="Add recipe" />
