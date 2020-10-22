3677defbea08a882b42cda225734c4d994f1c6b4
AndersRobstad <54552345+AndersRobstad@users.noreply.github.com>
2020-10-22 11:26:43 +0200

    add working form that posts the form-data to the backend

--- gr2038/ui/src/components/NewRecipe/NewRecipe.tsx
+++ gr2038/ui/src/components/NewRecipe/NewRecipe.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './newRecipe.scss';
 
 const NewRecipe = () => {
@@ -8,6 +9,10 @@ const NewRecipe = () => {
   const [ingredientNameField, setIngredientNameField] = useState('');
   const [ingredientQuantity, setIngredientQuantity] = useState('');
   const [unitField, setUnitField] = useState('none');
+  const [nameField, setNameField] = useState('');
+  const [portionsField, setPortionsField] = useState('');
+  const [timeField, setTimeField] = useState('');
+  const [descriptionField, setDescriptionField] = useState('');
 
   //Should maybe change these functions out with useState and having components for the ingredient list and instruction list.
   const addInstruction = () => {
@@ -21,6 +26,25 @@ const NewRecipe = () => {
         ...prevState,
         ingredientQuantity + ' ' + unitField + ', ' + ingredientNameField,
       ]);
+    } else {
+      //Add updating a state here that causes a feedback to render
+      //and use for example setTimeout to set the state back to empty when the
+      //feedback should be hidden again.
+      const ingredientNameFieldMessage = ingredientNameField
+        ? ''
+        : "ingredient can't be empty";
+      const ingredientQuantityMessage = ingredientQuantity
+        ? ''
+        : "quantity can't be empty";
+      const unitFieldMessage =
+        unitField !== 'none' ? '' : 'Unit must be chosen';
+      alert(
+        ingredientNameFieldMessage +
+          '\n' +
+          ingredientQuantityMessage +
+          '\n' +
+          unitFieldMessage
+      );
     }
   };
   const removeIngredient = (index: number) => {
@@ -34,14 +58,32 @@ const NewRecipe = () => {
     setInstructions(instructionsCopy);
   };
 
-  const submitNewRecipeForm = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    //Axiom post to the correct path
+  const submitNewRecipeForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (instructions.length && ingredients.length) {
+      //Change USERNAME to username of current user
+      axios.post('/users/USERNAME/recipes/add', {
+        name: nameField,
+        portions: portionsField,
+        time: timeField,
+        description: descriptionField,
+        instructions: instructions,
+        ingredients: ingredients,
+      });
+      window.location.reload();
+    } else {
+      const instructionsMessage =
+        instructions.length > 0 ? '' : 'You need to add instrucgtions \n';
+      const ingredientsMessage =
+        ingredients.length > 0 ? '' : 'You need to add ingredients';
+      alert(instructionsMessage + ingredientsMessage);
+    }
   };
   return (
     <div className="new-recipe-container">
       <form onSubmit={(e) => submitNewRecipeForm(e)}>
         <input
+          onChange={(e) => setNameField(e.target.value)}
           type="text"
           name="name"
           id="newRecipeForm"
@@ -50,8 +92,15 @@ const NewRecipe = () => {
         />
         <br />
         <label htmlFor="portions">Portions:</label>
-        <input type="number" name="portions" placeholder="num" required />
         <input
+          onChange={(e) => setPortionsField(e.target.value)}
+          type="number"
+          name="portions"
+          placeholder="num"
+          required
+        />
+        <input
+          onChange={(e) => setTimeField(e.target.value)}
           type="number"
           name="estimatedTime"
           placeholder="Estimated time"
@@ -59,6 +108,7 @@ const NewRecipe = () => {
         />
         <br />
         <textarea
+          onChange={(e) => setDescriptionField(e.target.value)}
           name="description"
           placeholder="description"
           required
@@ -80,11 +130,12 @@ const NewRecipe = () => {
         />
         <select
           onChange={(event) => setUnitField(event.target.value)}
+          defaultValue={'None'}
           name="unit"
           required
           placeholder="unit"
         >
-          <option value="none" selected disabled hidden>
+          <option value="None" disabled>
             Unit
           </option>
           <option value="gram">gram</option>
@@ -111,7 +162,7 @@ const NewRecipe = () => {
         </button>
         <br />
         <div className="instructions">
-          <p>Instructions</p>
+          <p id="feedback_text">Instructions</p>
           <ol>
             {instructions.map((instruction, index) => (
               <li key={index}>
