244ec973fcbd692d173186a8c706fde7141098f7
Børge Dreyer <borge.dreyer@gmail.com>
2020-10-28 16:25:02 +0100

    more housekeeping

--- gr2038/ui/src/pages/SettingsPage/SettingsPage.tsx
+++ gr2038/ui/src/pages/SettingsPage/SettingsPage.tsx
@@ -1,16 +1,23 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../components/UserContext/UserContext';
+import { useHistory } from 'react-router-dom';
 import './settingsPage.scss';
 
 const SettingsPage = () => {
   const { user, logOut } = useContext(UserContext);
+  const history = useHistory();
+
+  const handleLogout = () => {
+    history.push('/');
+    logOut();
+  };
   return (
     <section className="logout__container">
       <header className="logout__header">
         <h1>Hi, {user?.username}</h1>
         <h2>Are you sure you want to log out?</h2>
       </header>
-      <button onClick={logOut}>Logout</button>
+      <button onClick={handleLogout}>Logout</button>
     </section>
   );
 };
