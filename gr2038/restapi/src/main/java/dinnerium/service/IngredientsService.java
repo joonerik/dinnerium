18fd88cc30482252296dc418c0bbe4ef4fe01e88
AndersRobstad <54552345+AndersRobstad@users.noreply.github.com>
2020-11-02 13:50:40 +0100

    add javadoc and finish cleaning up the restapi

--- gr2038/restapi/src/main/java/dinnerium/service/IngredientsService.java
+++ gr2038/restapi/src/main/java/dinnerium/service/IngredientsService.java
@@ -10,12 +10,24 @@ public class IngredientsService {
     private final ObjectMapper mapper;
     private final UserService userService;
 
+    /**
+     * Instantiate a IngredientService object with a UserService to the userService field, and
+     * registers DinneriumModule as a module to the ObjectMapper.
+     */
     public IngredientsService() {
         mapper = new ObjectMapper();
         userService = new UserService();
         mapper.registerModule(new DinneriumModule());
     }
 
+    /**
+     * summary.
+     *
+     * @param requestBody the request body containing the ingredient to be added.
+     * @param username the username of the user where the ingredient should be added.
+     * @return the user on json format with the new ingredient added to it.
+     * @throws IOException if the ingredient is not on the correct json-format.
+     */
     public String addIngredient(String requestBody, String username) throws IOException {
         Ingredient ingredient = mapper.readValue(requestBody, Ingredient.class);
         if (ingredient == null) {
