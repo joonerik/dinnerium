897708ad199fac6add22197da14695b970dc6e14
AndersRobstad <54552345+AndersRobstad@users.noreply.github.com>
2020-11-15 15:35:53 +0100

    add package-info.java to all our packages with corresponding info

---
+++ dinnerium/core/src/main/java/dinnerium/core/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides the classes with all the core-logic for the Dinnerium application.
+ *
+ * @since 0.0.1-SNAPSHOT
+ */
+package dinnerium.core;
---
+++ dinnerium/restapi/src/main/java/dinnerium/server/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Provides the class needed to launch our RestServer with API endpoints.
+ *
+ * @since 0.0.1-SNAPSHOT
+ */
+package dinnerium.server;
